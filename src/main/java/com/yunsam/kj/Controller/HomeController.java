a309be184d43298a929291a8abc199d95037a30d
tongzhe2006 <71719683@qq.com>
2019-10-08 10:59:25 +0800

    testaaa

--- src/main/java/com/yunsam/kj/Controller/HomeController.java
+++ src/main/java/com/yunsam/kj/Controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping("/home")   //test
 public class HomeController {
     @RequestMapping(value = "/home",method = RequestMethod.GET)
     public String hello(Model model) {
